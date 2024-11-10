b1c76ba738f59fc9bf172a2df56c5743efe970b3
Daniel Mule <165652173+danmule3@users.noreply.github.com>
2024-11-10 13:52:45 +0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+# Program to greet the user with their name, age, and location  
+ 
+name = input("What is your name? :")  
+age = int(input("How old are you? :"))  
+location = input("Where are you located? :")  
+
+  
+print(f"Hello, {name}! You are {age} years old and you live in {location}. That's wonderful!" )
